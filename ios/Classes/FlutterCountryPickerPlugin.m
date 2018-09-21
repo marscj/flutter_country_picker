923b25cd9a6a52130ed068270ddaccb6d2ed4700
marscj <mjx_cj@163.com>
2018-09-22 01:30:53 +0400

    delete

--- ios/Classes/FlutterCountryPickerPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterCountryPickerPlugin.h"
-#import <flutter_country_picker/flutter_country_picker-Swift.h>
-
-@implementation FlutterCountryPickerPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterCountryPickerPlugin registerWithRegistrar:registrar];
-}
-@end
